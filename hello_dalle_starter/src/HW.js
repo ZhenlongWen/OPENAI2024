e327409e65e74a41834a6cd3dceced60831603c1
文朕龙 <wenzhenlong@abel.mynetworksettings.com>
2024-09-20 01:14:55 -0400

    homework

---
+++ hello_dalle_starter/src/HW.js
@@ -0,0 +1,34 @@
+import { say } from "./shared/cli.ts";
+import { promptDalle, promptGPT } from "./shared/openai.ts";
+
+say("Welcom to the future Gadget Designer store!");
+
+async function getGadgetInput() {
+  const purpose = await prompt("What is the purpose of the futuristic gadget?");
+  const features = await prompt("What are the key features of this gadget?");
+  
+  return { purpose, features };
+}
+
+
+function createGPTPrompt(details) {
+  return `
+    You are a futuristic engineer designing a gadget that serves the purpose of ${details.purpose}. 
+    The gadget includes the following key features: ${details.features}. 
+    Provide a brief description of how the gadget works and its appearance and key components according to the information provided above. 
+    Make the response futuristc, creative and in 70 words in one paragraph and only the description.
+    Your response should complete the sentence "The object is..."
+  `;
+}
+
+const gadgetDetails = await getGadgetInput();
+const promptToGPT = createGPTPrompt(gadgetDetails);
+const gptResponse = await promptGPT (promptToGPT, {temperature: 0.8, max_tokens: 512});
+;
+say("The Gadget Description is:");
+say(`${gptResponse}`);
+
+const imageResponse = await promptDalle(gptResponse);
+say(imageResponse.url);
+
+
